46d814602cb6e878018f0705b74ec8bb75af2958
DJ Roberts <djninjamaster13@gmail.com>
2021-12-19 15:20:29 -0500

    prod testing #1 create-scorecard redirect

--- src/components/create-scorecard.component.js
+++ src/components/create-scorecard.component.js
@@ -203,8 +203,8 @@ class CreateScorecard extends Component {
           })
 
         }
-
-        window.location = 'https://dreamy-mcnulty-8d46e1.netlify.app/scorecardlist';
+        //took this out for testing
+        //window.location = 'https://dreamy-mcnulty-8d46e1.netlify.app/scorecardlist';
     }
 
     render() {
